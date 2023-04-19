270d807ebbbe6c487c28283ad94e803a35cecd5b
leoweyr <2516427163@qq.com>
2023-04-19 13:49:25 +0800

    Fixed #1,#2; Update file:(new property<full_name>,property<name>,property<ext> of class<File>),doc

--- doc/API reference.md
+++ doc/API reference.md
@@ -3,12 +3,12 @@
 ### Constructor
 
 ```python
-File(filePath)
+File(file_path)
 ```
 
 | Parameter | Type   | Default | Description                                                  |
 | --------- | ------ | ------- | ------------------------------------------------------------ |
-| filePath  | string |         | The absolute path or relative path of the file. If using a relative path, it must include `.` or `/`. |
+| file_path | string |         | The absolute path or relative path of the file. If using a relative path, it must include `.` or `/`. |
 
 ### Attributes
 
@@ -17,18 +17,31 @@ File(filePath)
     	<td><code>File.content</code></td>
         <td>Return the entire content of the file.</td>
     </tr>
+    <tr>
+    	<td><code>File.full_name</code></td>
+        <td>Return the name of the file.</td>
+    </tr>
+    <tr>
+    	<td><code>File.name</code></td>
+        <td>Return the name of the file without the file extension.</td>
+    </tr>
+    <tr>
+    	<td><code>File.ext</code></td>
+        <td>Return the file extension of the file.</td>
+    </tr>
 </table>
+
 
 ### Operation
 
-#### File.Rewrite(content)
+#### File.rewrite(content)
 
 Rewrite the entire content of the file. 
 
-#### File.Append(content)
+#### File.append(content)
 
 Append content at the end of file content.
 
-#### File.Delete()
+#### File.delete()
 
 Delete file.
--- easierfile/file.py
+++ easierfile/file.py
@@ -1,35 +1,50 @@
 import os
 
 class File:
-    def __init__(self, filePath):
-        self.__m_filePath = filePath
-        self.__m_dirPath = os.path.dirname(self.__m_filePath)
-        if os.path.exists(self.__m_dirPath) == False:  # If the directory of the file path does not exist, it will be created.
-            os.mkdir(self.__m_dirPath)
+    def __init__(self, file_path):
+        self.__m_file_path = file_path
+        self.__m_file_full_name = os.path.basename(self.__m_file_path)
+        self.__m_file_name, self.__m_file_extension = os.path.splitext(self.__m_file_full_name)
+        self.__m_dir_path = os.path.dirname(self.__m_file_path)
+
+        if os.path.exists(self.__m_dir_path) == False:  # If the directory of the file path does not exist, it will be created.
+            os.mkdir(self.__m_dir_path)
         try:  # If the file does not exist, it will be created.
-            self.__m_file = open(self.__m_filePath, "x")
-        except:
-            pass
+            self.__m_file = open(self.__m_file_path, "x")
+        except FileExistsError:
+            pass  # Indicates that the file exists and no further exception handling is required.
         else:
             self.__m_file.close()
 
     @property
     def content(self):
-        self.__m_file = open(self.__m_filePath, "r")
+        self.__m_file = open(self.__m_file_path, "r")
         self.__m_content = self.__m_file.read()
         self.__m_file.close()
         return self.__m_content
 
-    def Rewrite(self,content):
-        self.__m_file = open(self.__m_filePath, "w")
+    @property
+    def full_name(self):
+        return self.__m_file_full_name
+
+    @property
+    def name(self):
+        return self.__m_file_name
+
+    @property
+    def ext(self):
+        return self.__m_file_extension.split(".")[-1]
+
+    def rewrite(self,content):
+        self.__m_file = open(self.__m_file_path, "w")
         self.__m_file.write(content)
         self.__m_file.close()
 
-    def Append(self,content):
-        self.__m_file = open(self.__m_filePath, "a")
+    def append(self,content):
+        self.__m_file = open(self.__m_file_path, "a")
         self.__m_file.write(content)
         self.__m_file.close()
 
-    def Delete(self):
-        if os.path.exists(self.__m_filePath):
-            os.remove(self.__m_filePath)
+    def delete(self):
+        if os.path.exists(self.__m_file_path):
+            os.remove(self.__m_file_path)
